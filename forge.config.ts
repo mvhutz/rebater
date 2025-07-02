13f81cf222e09b92139101324ec8fa037c418826
Max <git@maximhutz.me>
2025-07-02 03:57:25 -0400

    feat: actually good icon

--- forge.config.ts
+++ forge.config.ts
@@ -9,6 +9,8 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
 const config: ForgeConfig = {
   packagerConfig: {
+    name: "Rebater",
+    executableName: "Rebater",
     asar: true,
     icon: './images/icon'
   },
