9d36d47d615e2547afe770985663c09994ddf0c9
M. V. Hutz <pannus@maximhutz.me>
2025-08-06 20:17:38 -0400

    feat: linked documentation

--- src/client/pages/view/tabs/documentation/DocumentationTab.tsx
+++ src/client/pages/view/tabs/documentation/DocumentationTab.tsx
@@ -6,10 +6,8 @@ import { Sheet, Typography } from '@mui/joy';
 import TabMenu from '../../TabMenu';
 import { useAppSelector } from '../../../../../client/store/hooks';
 import { getDisplayTab } from '../../../../../client/store/slices/ui';
-import { HashLink } from 'react-router-hash-link';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
-import path from 'path-browserify';
 
 /** ------------------------------------------------------------------------- */
 
@@ -17,9 +15,15 @@ const MARKDOWN_COMPONENTS: Components = {
   a({ node, href, ...rest }) {
     void [node];
 
-    return <HashLink scroll={e => e.scrollIntoView({ "behavior": "smooth", block: 'center'})} to={href?.[0] === "#" ? href : path.join("..", href ?? "/")} {...rest} />
+    return <a href={href} target="_blank" {...rest} />
   }
 };
+
+const DOCUMENTATION_PAGE = `
+# Documentation
+
+You can learn more about this program [here](https://github.com/mvhutz/rebater#-rebater).
+`;
 
 /** ------------------------------------------------------------------------- */
 
@@ -32,11 +36,7 @@ function DocumentationTab() {
         <Typography level="body-lg" pt={0.5} color="neutral"><i>Documentation</i></Typography>
       </TabMenu>
       <Sheet sx={{ p: 5, overflow: "scroll" }}>
-        <Markdown components={MARKDOWN_COMPONENTS} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeSlug, rehypeAutolinkHeadings]}>
-          # Documentation
-
-          You can learn more about this program [here](https://github.com/mvhutz/rebater#-rebater)
-        </Markdown>
+        <Markdown components={MARKDOWN_COMPONENTS} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeSlug, rehypeAutolinkHeadings]} children={DOCUMENTATION_PAGE}/>
       </Sheet>
     </Stack>
   );
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, shell } from 'electron';
 import path from 'path';
 import started from 'electron-squirrel-startup';
 import { IPCHandler } from './shared/ipc/handler';
@@ -27,6 +27,11 @@ async function createWindow() {
   // Handle interaction between threads.
   const handler = new IPCHandler(mainWindow);
   handler.handleIPC();
+
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
 
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
